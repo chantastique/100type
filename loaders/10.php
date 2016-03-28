5a0f9aae8ff7372fa3689f0774fec7a88a11fe06
Chantal Jandard <chantaljandard@gmail.com>
2016-03-27 20:58:41 -0700

    8, 9, 10
    slow ’n steady

---
+++ loaders/10.php
@@ -0,0 +1,3 @@
+<link href="styles/css/010.css" media="screen" rel="stylesheet" />
+<script src="https://use.typekit.net/bhs8wjy.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
---
+++ loaders/8.php
@@ -0,0 +1,3 @@
+<link href="styles/css/008.css" media="screen" rel="stylesheet" />
+<!--<script src="https://use.typekit.net/yqf5iwv.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>-->
